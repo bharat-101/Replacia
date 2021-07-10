effed6ed5f150f2e213730f16502e6f10f7deb23
Bharat Lalwani <75477289+bharat-101@users.noreply.github.com>
2021-07-10 17:24:59 +0530

    Add files via upload

--- content_script.js
+++ content_script.js
@@ -1,15 +1,15 @@
 const a = document.createElement('a');
-chrome.storage.sync.get(['name','color'],function(data){
+chrome.storage.sync.get(['name','color1','color2'],function(data){
     const name = data.name;
     a.innerHTML = name;
-    const setcolor = data.color;
-    console.log(setcolor);
-    a.setAttribute('style',"font-size: 70px; margin: 30px; font-family: cursive; font-weight:bold; color:"+setcolor+";");
+    const setstartcolor = data.color1;
+    const setendcolor = data.color2;
+    console.log(setstartcolor);
+    a.setAttribute('style',"font-size: 70px; margin: 30px; font-family: cursive; font-weight:bold; background:" +setstartcolor+";background: -webkit-linear-gradient(to right, "+setstartcolor+" 0%, "+setendcolor+" 100%);background: -moz-linear-gradient(to right, "+setstartcolor+" 0%, "+setendcolor+" 100%);background: linear-gradient(to right, "+setstartcolor+" 0%, "+setendcolor+" 100%);-webkit-background-clip: text;-webkit-text-fill-color: transparent;");
     var fullurl = chrome.extension.getURL('/popup.html');
     console.log(fullurl);
     a.setAttribute("href",fullurl);
 });
-
 
 const t = document.getElementsByClassName('L3eUgb');
 const tchild = t[0].getElementsByClassName('o3j99 LLD4me yr19Zb LS8OJ');
--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 function work(){
     var name = $('#name').val();
-    var col = $('#color').val();
-    chrome.storage.sync.set({'name': name, 'color': col});
-    $('#greet').text('Hey, '+name);
+    var col1 = $('#color1').val();
+    var col2 = $('#color2').val();
+    chrome.storage.sync.set({'name': name, 'color1': col1,'color2': col2});
     chrome.tabs.update({url:'background.html'});
     
 }
